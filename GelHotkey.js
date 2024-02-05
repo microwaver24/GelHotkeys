8e2a0c3566690e9735c4fe7baf918c4fe4b36e13
microwaver24 <microwaver24@gmail.com>
2024-02-04 21:54:28 -0800

    Replace input handling with hotkeys-js.
    Why am I writing input handling from scratch? There are tons of packages to wrap it up and make it easier to use. I went with https://github.com/jaywcjlove/hotkeys-js.

--- GelHotkey.js
+++ GelHotkey.js
@@ -7,6 +7,7 @@
 // @match        *://gelbooru.com/index.php?page=post&s=view*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=gelbooru.com
 // @grant        none
+// @require      https://unpkg.com/hotkeys-js/dist/hotkeys.min.js
 // ==/UserScript==
 
 // Hotkeys:
@@ -17,17 +18,43 @@
 // Previous post (while focusing video): shift + arrow left or shift + numpad 4 with numlock off
 // Next post (while focusing video): shift + arrow right or shift + numpad 6 with numlock off
 
+// import hotkeys from "hotkeys-js";
+
 (function () {
     "use strict";
 
-    let _enableLogs = false;
+    let _enableLogs = true;
+
+    hotkeys("b,num_0", function (event, handler) {
+        addFavorite(event, handler);
+    });
+
+    hotkeys("shift+b,num_1", function (event, handler) {
+        removeFavorite(event, handler);
+    });
+
+    hotkeys("v,num_4", function (event, handler) {
+        toggleVideoFocus(event, handler);
+    });
+
+    hotkeys("space,num_5", function (event, handler) {
+        toggleVideoPlay(event, handler);
+    });
+
+    hotkeys("shift+left", function (event, handler) {
+        navigatePrev(event, handler);
+    });
+
+    hotkeys("shift+right", function (event, handler) {
+        navigateNext(event, handler);
+    });
 
     function getImageId() {
         const params = new URLSearchParams(window.location.search);
         return params.get("id");
     }
 
-    function addFavorite() {
+    function addFavorite(event, handler) {
         const imageId = getImageId();
         window.post_vote(imageId, "up");
         window.addFav(imageId);
@@ -39,7 +66,7 @@
         return true;
     }
 
-    function removeFavorite() {
+    function removeFavorite(event, handler) {
         const imageId = getImageId();
         let url = new URL("/index.php", "https://gelbooru.com");
         url.searchParams.append("page", "favorites");
@@ -62,7 +89,7 @@
         return document.getElementsByTagName("video")[0];
     }
 
-    function toggleVideoFocus() {
+    function toggleVideoFocus(event, handler) {
         let video = getVideo();
         if (!(video instanceof HTMLVideoElement)) {
             return false;
@@ -81,9 +108,9 @@
         return true;
     }
 
-    function toggleVideoPlay(e) {
+    function toggleVideoPlay(event, handler) {
         // If we are focusing the video, let the video's normal handling do its thing.
-        if (e.target instanceof HTMLVideoElement) {
+        if (event.target instanceof HTMLVideoElement) {
             return false;
         }
 
@@ -116,8 +143,8 @@
         video.play();
     }
 
-    function navigatePrev(e) {
-        if (!(e.target instanceof HTMLVideoElement)) {
+    function navigatePrev(event, handler) {
+        if (!(event.target instanceof HTMLVideoElement)) {
             return false;
         }
 
@@ -125,105 +152,14 @@
         return true;
     }
 
-    function navigateNext(e) {
-        if (!(e.target instanceof HTMLVideoElement)) {
+    function navigateNext(event, handler) {
+        if (!(event.target instanceof HTMLVideoElement)) {
             return false;
         }
 
         window.navigateNext();
         return true;
     }
-
-    function handleInput(e) {
-        if (_enableLogs) {
-            console.log(
-                `handleInput:` +
-                    ` code [${e.code}]` +
-                    ` key [${e.key}]` +
-                    ` keyCode (deprecated) [${e.keyCode}]` +
-                    ` altKey [${e.altKey}]` +
-                    ` ctrlKey [${e.ctrlKey}]` +
-                    ` metaKey [${e.metaKey}]` +
-                    ` shiftKey [${e.shiftKey}]` +
-                    ` target [${e.target}]`,
-            );
-        }
-
-        let inputIsHandled = false;
-
-        // Check keys by location.
-        switch (e.code) {
-            // Acccept only the numpad and not the number keys since it's conveniently close to the arrow keys.
-            case "Numpad0":
-                // Make sure numlock is on.
-                if (e.key === "0") {
-                    inputIsHandled = addFavorite();
-                }
-                break;
-            // Acccept only the numpad and not the number keys since it's conveniently close to the arrow keys.
-            case "Numpad1":
-                // Make sure numlock is on.
-                if (e.key === "1") {
-                    inputIsHandled = removeFavorite();
-                }
-                break;
-            case "Space":
-                inputIsHandled = toggleVideoPlay(e);
-                break;
-        }
-
-        if (!inputIsHandled) {
-            // Check keys by name.
-            switch (e.key) {
-                case "b": // "b" for "bookmark".
-                    inputIsHandled = addFavorite();
-                    break;
-                case "B": // "shift + b" to do the reverse.
-                    inputIsHandled = removeFavorite();
-                    break;
-                case "v": // "v" for "video".
-                    inputIsHandled = toggleVideoFocus();
-                    break;
-
-                // Arow keys are handled by `key` instead of `code` so that you can use the numpad with numlock off,
-                // since that works in the existing Gelbooru hotkeys.
-                case "ArrowLeft":
-                    if (e.shiftKey) {
-                        inputIsHandled = navigatePrev(e);
-                    }
-                    break;
-                case "ArrowRight":
-                    if (e.shiftKey) {
-                        inputIsHandled = navigateNext(e);
-                    }
-                    break;
-            }
-        }
-
-        if (inputIsHandled) {
-            e.preventDefault();
-            e.stopPropagation();
-        }
-
-        return inputIsHandled !== true;
-    }
-
-    // Using `onkeydown` instead of `onkeypress` so that I can detect the arrow keys.
-    parent.onkeydown = function (e) {
-        // If you type in a text box, disable all input handling.
-        // You'll have to click something besides the text box to get the hotkeys back.
-        if (e.target instanceof HTMLInputElement) {
-            return;
-        }
-
-        let inputResult = handleInput(e);
-
-        if (inputResult !== false) {
-            inputResult = handleInput(window.event);
-        }
-
-        return inputResult;
-    };
 
     // todo: maybe I can just set the video to start playing right away so I don't need to focus or unfocus it.
     // autoPlayVideo();
